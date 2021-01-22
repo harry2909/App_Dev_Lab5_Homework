14e4ca0d7e7a90f685249d576e8cc17f2f561e21
Harry <hpreston2909@gmail.com>
2021-01-22 16:37:47 +0000

    Instance is saved on rotate.

--- app/src/main/java/com/example/app_dev_lab5_homework/MainActivity.java
+++ app/src/main/java/com/example/app_dev_lab5_homework/MainActivity.java
@@ -4,26 +4,87 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.graphics.Color;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     private int mCount = 0;
     private TextView mShowCount;
+    private EditText textEdit;
+    private static final String LOG_TAG = MainActivity.class.getSimpleName();
+    public static String savedStateValue;
+    public static String getCount;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         mShowCount = (TextView) findViewById(R.id.count_view);
+        textEdit = (EditText) findViewById(R.id.edit_text);
+
+        if (savedInstanceState != null) {
+            textEdit.setText(savedInstanceState.getString("editText_text"));
+            mShowCount.setText(savedInstanceState.getString("count_text"));
+        }
+
+
     }
 
     public void countUp(View view) {
         mCount++;
-        if(mShowCount != null){
+        if (mShowCount != null) {
             mShowCount.setText(Integer.toString(mCount));
         }
 
     }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d(LOG_TAG, "onStart");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d(LOG_TAG, "onPause");
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.d(LOG_TAG, "onRestart");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d(LOG_TAG, "onResume");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d(LOG_TAG, "onStop");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d(LOG_TAG, "onDestroy");
+    }
+
+    // check to see if header is visible then put that state in bundle
+    @Override
+    public void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        savedStateValue = textEdit.getText().toString();
+        getCount = mShowCount.getText().toString();
+        outState.putString("editText_text", savedStateValue);
+        outState.putString("count_text", getCount);
+    }
+
 }
